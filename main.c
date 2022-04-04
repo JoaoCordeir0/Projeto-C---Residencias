b90de3a224be495b6f5563892e6ee711664586cc
Joao.Cordeiro <joaocordeiro2134@gmail.com>
2022-04-04 16:05:13 -0300

    Add estrutura

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <locale.h>
+
+int main(int argc, char *argv[]) {
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	// LeituraDados();
+	printf("teste");
+	
+	return 0;
+}
